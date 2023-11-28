9bdd7d21a6b535470306696094f32846c881b21c
jho5245 <jho5245@naver.com>
2023-11-28 22:00:04 +0900

    Change CommandRide.java 2

--- src/main/java/com/jho5245/cucumbery/commands/brigadier/CommandRide.java
+++ src/main/java/com/jho5245/cucumbery/commands/brigadier/CommandRide.java
@@ -68,7 +68,9 @@ public class CommandRide extends CommandBase
 
 	private boolean ride(@NotNull Entity passenger, @NotNull Entity vehicle)
 	{
-		if (vehicle instanceof Player && (passenger instanceof Zombie || passenger instanceof Skeleton))
+		if (vehicle instanceof Player && (passenger instanceof Skeleton || passenger instanceof Spider || passenger instanceof EnderDragon
+				|| passenger instanceof Giant || passenger instanceof PiglinAbstract || passenger instanceof Illager || (passenger instanceof Zombie
+				&& !(passenger instanceof ZombieVillager))))
 		{
 			AreaEffectCloud areaEffectCloud = vehicle.getWorld().spawn(vehicle.getLocation(), AreaEffectCloud.class, cloud ->
 			{
